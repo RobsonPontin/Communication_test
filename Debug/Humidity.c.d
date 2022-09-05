29f058eec5c231e6abef63e41403c8458b42e73c
Robson Pontin <66638512+pontin-pdftron@users.noreply.github.com>
2022-09-05 14:41:04 -0700

    Cleanup of debug and build files

--- Debug/Humidity.c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Robson\AppData\Local\Temp\VMBuilds\Communication_test\mega_atmega2560\Debug\Humidity.c.o: \
- C:\Users\Robson\AppData\Local\Temp\VMBuilds\Communication_test\mega_atmega2560\Debug\Humidity.c \
- C:\Users\Robson\AppData\Local\Temp\VMBuilds\Communication_test\mega_atmega2560\Debug\Humidity.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h
